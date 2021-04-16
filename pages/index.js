f297c6143f87251ea6158d4e2bccf0b776beb8f7
Jess Carmona <jess.carmona.ascarrunz@gmail.com>
2021-04-16 10:14:07 -0300

    updates pages

--- pages/index.js
+++ pages/index.js
@@ -6,25 +6,9 @@ function Home() {
         <div> 
             <h3>Home 2</h3>
             <a href="/sobre">Acessar página 'sobre'</a>
-            <Contador/>
         </div>
     )
     
-}
-
-function Contador() {
-    const [contador, setContador] = useState(1);
-
-    function addContador(){
-        setContador(contador + 1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={addContador}>Adicionar</button>
-        </div>
-    )
 }
 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,13 @@
+import { useState } from 'react';
+
+function Sobre() {
+    return (
+        <div> 
+            <h3>Sobre</h3>
+            <a href="/home">Acessar página 'Home'</a>
+        </div>
+    )
+    
+}
+
+export default Sobre
