d5e19782bc7c5c2fd735137e2036be47a0f81be8
Jess Carmona <jess.carmona.ascarrunz@gmail.com>
2021-04-16 10:18:09 -0300

    updating

--- pages/sobre.js
+++ pages/sobre.js
@@ -4,7 +4,7 @@ function Sobre() {
     return (
         <div> 
             <h3>Sobre</h3>
-            <a href="/home">Acessar página 'Home'</a>
+            <a href="/">Acessar página 'Home'</a>
         </div>
     )
     
